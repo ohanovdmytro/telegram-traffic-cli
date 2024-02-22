feecba90d3ef7f27209f7c6ab0cfbe70d171b3c7
Dmytro Ohanov <dmitryohanovphotography@gmail.com>
2024-02-22 21:50:04 +0000

    update made for a gitignore

--- in.js
+++
@@ -1,5 +0,0 @@
-// sets the mode to HTML 
-; await client.sendMessage("me",{message:"This is an underline text"}); 
-// disable formatting 
-client.setParseMode(undefined); 
-await client.sendMessage("me",{message:" this will be sent as it is ** with no formatting **});
